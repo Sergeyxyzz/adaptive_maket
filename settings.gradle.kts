3776454d8e6330a2c586291a540b36f0dfb95796
Sergey <sergeyxyzz@yandex.ru>
2023-09-14 11:35:53 +0300

    rdy

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MyConstraint Layout 2"
+include(":app")
+ 
